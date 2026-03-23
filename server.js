948a1912de4234ec97d76c9936fdbcf5ad6f5f52
Pooja Chaudhary <pooja.chaudhary0080@gmail.com>
2026-03-23 05:45:13 +0000

    backend setup

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
 
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/contact", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://poojachaudhary.infinityfree.me/myapi/api/saveContact.php",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams(req.body),
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend working 🚀");
+});
+
+app.listen(5000, () => console.log("Server running on 5000"));
